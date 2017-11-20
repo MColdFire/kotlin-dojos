b3903093adc5ae05f4f81042bdb9fcd3dc13d9d6
Taoufik <taoufik.benayad@gmail.com>
2017-11-20 22:59:31 +0100

    typealias

--- ui-search/src/main/kotlin/Play.kt
+++ ui-search/src/main/kotlin/Play.kt
@@ -1,14 +1,15 @@
-data class Money(val value: Int)
+typealias Currency = String
+
+data class Money(val value: Int, val currency: Currency)
 
 fun currencyPair(): Pair<Money, Money> {
-    return Pair(Money(1), Money(100))
+    return Pair(Money(1, "EUR"), Money(100,"USD"))
 }
-
 object SingletonRates {
     val exchangeRate = 100
 }
 
 fun main(args: Array<String>) {
-    println(SingletonRates.exchangeRate)
-
+    val currencyPair = currencyPair()
+    println(currencyPair)
 }
