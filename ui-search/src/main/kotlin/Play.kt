570c24b765f0f318e485da454c5ea8c378986df7
Taoufik <taoufik.benayad@gmail.com>
2017-11-20 23:07:10 +0100

    if/else is an expression + pattern matching

--- ui-search/src/main/kotlin/Play.kt
+++ ui-search/src/main/kotlin/Play.kt
@@ -11,5 +11,25 @@ object SingletonRates {
 
 fun main(args: Array<String>) {
     val currencyPair = currencyPair()
-    println(currencyPair)
+    val currency = if (currencyPair.first == Money(1, "EUR")) "EUR" else "N/A";
+    println(currency)
+
+    val value = when(currencyPair.second.value) {
+        1 -> {
+            "One"
+        }
+        100 -> {
+            "Hundred"
+        }
+        20,80 -> {
+            "20 or 80"
+        }
+        in 30..50 -> {
+            "30 to 50"
+        }
+        else -> {
+            "Don't care"
+        }
+    }
+    println(value)
 }
